572025cfd8350faa7f20f0774a2018a9d2be5133
ngthmduyen <nguyenduyen.231006@gmail.com>
2025-02-22 11:51:53 +0700

    models

---
+++ DailySumary.py
@@ -0,0 +1,3 @@
+class DailySummary:
+    def __init__(self, material_manager):
+        self.material_manager = material_manager
---
+++ ListMaterial.py
@@ -0,0 +1,3 @@
+#lớp này dùng để thực hiện các theo tác thêm, xoá, lọc, tìm,...sản phẩm
+class ListMaterial:
+    def __init__(self, data_connector):
