e13032ba079905b6d45cfde56ab0bb0e78c93856
Thien An <annttk24406h2@st.uel.edu.vn>
2025-02-23 16:23:50 +0700

    Update DataConnector.py

--- DataConnector.py
+++ DataConnector.py
@@ -1,1 +1,6 @@
 class DataConnector:
+      def get_all_materials(self):
+        jff=JsonFileFactory()
+        filename='../dataset/testdoan.json'
+        materials=jff.read_data(filename,Material)
+        return materials
