7e9ed7d3790306a1d9bbac385ca40ba19fb7c629
Trinh186 <trinhtttk24406h@st.uel.edu.vn>
2025-03-20 23:12:39 +0700

    Update LoginMainWindowExt.py

--- ui/LoginMainWindowExt.py
+++ ui/LoginMainWindowExt.py
@@ -12,21 +12,35 @@ class LoginMainWindowExt(Ui_MainWindow):
         self.radioButtonManager.clicked.connect(self.show_manager)
         self.radioButtonEmployee.clicked.connect(self.show_material_management)
     def process_login(self):
-        dc=DataConnector()
-        uid = self.lineEditUserName.text()
-        pwd = self.lineEditPassword.text()
-        emp = dc.login(uid, pwd)
-        if emp != None:
-            self.MainWindow.close()#close login window
+    #hàm kiểm tra thông tin nhập người dùng và xác định vai trò user
+        username = self.lineEditUserName.text()
+        password = self.lineEditPassword.text()
+        if not username or not password:
+            QMessageBox.warning(self.MainWindow, "Lỗi", "Vui lòng nhập đầy đủ username và password!")
+            return
+            # Xác định vai trò từ radio button
+        if self.radioButtonEmployee.isChecked():
+            role = "Employee"
+        elif self.radioButtonManager.isChecked():
+            role = "Manager"
+        else:
+            QMessageBox.warning(self.MainWindow, "Lỗi", "Vui lòng chọn vai trò!")
+            return
+        # Kết nối database
+        dc = DataConnector()
+        user = dc.login(username, password, role)  # Truy vấn user theo role => thuc hiện login
+        #nếu thành công
+        if user:
+            self.MainWindow.close()
             self.mainwindow = QMainWindow()
-            self.myui = MaterialManagement()
+            self.myui = MaterialManagementExt(role) #mo cửa sổ nguyên liệu theo vai trò
             self.myui.setupUi(self.mainwindow)
             self.myui.showWindow()
         else:
-            self.msg = QMessageBox(self.MainWindow)
-            self.msg.setText("Đăng nhập thất bại")
-            self.msg.exec()
+            QMessageBox.warning(self.MainWindow, "Lỗi", "Tên đăng nhập hoặc mật khẩu không đúng!")
+
     def process_exit(self):
+    #button exit
         msgbox = QMessageBox(self.MainWindow)
         msgbox.setText("Chắc chắn thoát?")
         msgbox.setWindowTitle("Xác nhận thoát")
